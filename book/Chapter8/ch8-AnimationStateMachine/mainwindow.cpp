b088642747567113e13f38aa27fb351df50f7686
krlee <rukamlee@aten.com.tw>
2020-10-23 10:55:41 +0800

    Bugfix: wrong logic

--- book/Chapter8/ch8-AnimationStateMachine/mainwindow.cpp
+++ book/Chapter8/ch8-AnimationStateMachine/mainwindow.cpp
@@ -19,17 +19,19 @@ MainWindow::MainWindow(QWidget *parent)
 
     QState *state1 = new QState(machine);
     state1->assignProperty(button, "geometry", QRect(10,10,100,30));
-//    machine->setInitialState(state1);
 
     QState *state2 = new QState(machine);
-    state1->assignProperty(button, "geometry", QRect(250,250,100,30));
+    state2->assignProperty(button, "geometry", QRect(250,250,100,30));
 
     QSignalTransition *transition1 = state1->addTransition(button, SIGNAL(clicked()), state2);
     transition1->addAnimation(new QPropertyAnimation(button, "geometry"));
+    state1->addTransition(transition1);
 
     QSignalTransition *transition2 = state2->addTransition(button, SIGNAL(clicked()), state1);
     transition2->addAnimation(new QPropertyAnimation(button, "geometry"));
+    state2->addTransition(transition2);
 
+    machine->setInitialState(state1);
     machine->start();
 }
 
